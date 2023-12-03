dd8e4f25369f9d868d196e9580f00b4e98ac264b
YadavShashank36 <shashankyadavrocks@gmail.com>
2023-12-03 23:08:42 +0530

    added first enpoint get all books

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const connectToMongo =require('./db');
-connectToMongo(); 
-const express = require('express')
-const app = express()
-const port=3000
+const connectToMongo = require('./db');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
+app.use(express.json());
+app.use(express.static("public"));
 
-   app.use(express.json());
-//available routes
-//app.use('/api/auth',require('./routes/auth.js'))
-//app.use('/api/notes', require('./routes/notes.js'))
+// Remove this line if "Book" is not being used
+const Book = require("./book.js");
 
-app.listen(port,()=>
- console.log(`example app is listen on port ${port}`))
+// Define your router
+const router = express.Router();
+
+router.get('/GET/api/books', async (req, res) => {
+    try {
+        const notes = await Book.find({});
+        res.json(notes);
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).send("some error");
+      }
+});
+
+// Tell the app to use your router for routes starting with '/'
+app.use('/GET/api/books', router);
+
+app.listen(port, () => {
+    console.log(`Example app is listening on port ${port}`);
+});
